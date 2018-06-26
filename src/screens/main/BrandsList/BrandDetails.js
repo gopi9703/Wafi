1c3115dc77f74a76f5eb55adf483f42a8afc482e
gopiy <gopiy@PRINHYLTPAP1162.local>
2018-06-26 15:54:21 +0530

    share and placeholders implemented

--- src/screens/main/BrandsList/BrandDetails.js
+++ src/screens/main/BrandsList/BrandDetails.js
@@ -1,66 +1,90 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, Alert, TouchableOpacity,ActivityIndicator, Dimensions, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, Image, Alert, TouchableOpacity, ActivityIndicator, Dimensions, ScrollView, Linking } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 
 class BrandDetails extends Component {
-  constructor(props) {
-      super(props)
-      this.state = {
-          isLoading: true,
-          dataSource: [],
-      }
-  }
-  componentDidMount() {
-      return fetch("http://admin.wafideals.com/apioffers/"+this.props.offerid, {method: "GET",headers: {
-                         'Accept': 'application/json',
-                         'Content-Type': 'application/json',
-                     }})
-          .then((response) => response.json())
-          .then((responseJson) => {
-              this.setState({
-                  isLoading: false,
-                  dataSource: (responseJson),
-              })
-          })
-          .catch((error) => {
-              console.error(error)
-          })
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoading: true,
+            dataSource: [],
+        }
+    }
+
+    callNumber = (url) =>{
+        Linking.canOpenURL(url).then(supported => {
+        if (!supported) {
+         console.log('Can\'t handle url: ' + url);
+        } else {
+         return Linking.openURL(url);
+        }
+      }).catch(err => console.error('An error occurred', err));
+     }
+
+    componentDidMount() {
+        return fetch("http://admin.wafideals.com/apioffers/" + this.props.offerid, {
+            method: "GET", headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    isLoading: false,
+                    dataSource: (responseJson),
+                })
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }
     render() {
-      if (this.state.isLoading) {
-        return (
+        if (this.state.isLoading) {
+            return (
 
-          <View style={{ flex: 1, padding: 20, alignItems: 'center', justifyContent: 'center' }}>
-            <ActivityIndicator size="large" />
-          </View>
+                <View style={{ flex: 1, padding: 20, alignItems: 'center', justifyContent: 'center' }}>
+                    <ActivityIndicator size="large" />
+                </View>
 
-        );
-      }
+            );
+        }
         return (
             <View style={styles.bodyBg}>
                 <ScrollView>
-                <View style={styles.BrandIntro}>
-                    <View>
-                        <Image source={{ uri: 'http://admin.wafideals.com/storage/' + this.state.dataSource.logo_path  }} style={styles.BrandLogo} />
-                    </View>
-                    <View style={styles.infoWrpr}>
+                    <View style={styles.BrandIntro}>
+                        <View>
+                            <Image source={{ uri: 'http://admin.wafideals.com/storage/' + this.state.dataSource.logo_path }} style={styles.BrandLogo} />
+                        </View>
+                        <View style={styles.infoWrpr}>
+                            <TouchableOpacity onPress={()=> this.callNumber(`tel:+19742223645`)}>
+                                <View style={styles.IconBlk}>
+                                    <Icon name="ios-call" size={24} color="#ffffff" style={styles.iconStyler} />
+                                     <Text style={[styles.iconText]}>Contact</Text>
+                                </View>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <View style={styles.IconBlk}>
+                                    <Icon name="ios-pin" size={24} color="#ffffff" style={[styles.iconStyler, styles.location]} />
+                                    <Text style={[styles.iconText]}>Location</Text>
+                                </View>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <View style={styles.IconBlk}>
+                                    <Icon name="md-share" size={22} color="#ffffff" style={[styles.iconStyler, styles.share]} />
+                                    <Text style={styles.iconText}>Share</Text>
+                                </View>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <View style={styles.IconBlk}>
+                                    <Icon name="ios-heart" size={22} color="#ffffff" style={[styles.iconStyler, styles.favorite]} />
+                                    <Text style={styles.iconText}>My Fav</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
 
-                        <TouchableOpacity>
-                            <View style={styles.IconBlk}>
-                                <Icon name="md-share" size={22} color="#ffffff" style={[styles.iconStyler, styles.share]} />
-                                <Text style={styles.iconText}>Share</Text>
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity>
-                            <View style={styles.IconBlk}>
-                                <Icon name="ios-heart" size={22} color="#ffffff" style={[styles.iconStyler, styles.favorite]} />
-                                <Text style={styles.iconText}>My Fav</Text>
-                            </View>
-                        </TouchableOpacity>
                     </View>
-
-                </View>
                     <Image source={{ uri: 'http://admin.wafideals.com/storage/' + this.state.dataSource.banner_logo_path }} style={styles.BrandBanner} />
                     <View style={styles.brandInfo}>
                         <Text style={styles.brandHdr}>{this.state.dataSource.short_description}</Text>
@@ -95,14 +119,14 @@ const styles = StyleSheet.create({
         color: '#000000',
         fontFamily: "MyriadPro-Semibold_2",
         lineHeight: 20,
-        paddingBottom : 5,
+        paddingBottom: 5,
     },
     mallDescTxt: {
         color: '#58595B',
         flex: 1,
         flexWrap: 'wrap',
-        fontSize : 13,
-        lineHeight : 17,
+        fontSize: 13,
+        lineHeight: 17,
     },
     BrandIntro: {
         backgroundColor: '#ffffff',
@@ -158,7 +182,7 @@ const styles = StyleSheet.create({
     },
     share: {
         paddingHorizontal: 9,
-        paddingVertical : 6,
+        paddingVertical: 6,
     },
 });
 
